be9b3f55736c8848610cecde6b89b7ac2ab95c42
rupom <furqanrupom978@gmail.com>
2023-11-22 22:54:10 +0600

    Project setup done

---
+++ src/server.ts
@@ -0,0 +1,16 @@
+import app from "./app"
+import config from "./app/config";
+
+const main = async () => {
+    try {
+        app.listen(config.port,() => {
+            console.log(`The app is running on port ${config.port}`)
+        })
+
+    } catch (error) {
+       console.log(error)
+    }
+}
+
+main();
+
